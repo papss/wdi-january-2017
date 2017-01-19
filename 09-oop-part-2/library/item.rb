4ad616c77bf4aafd549133c5b7b1c4cba1ede746
Bitmaker Labs <bitmakerlabs@Bitmaker-3.local>
2017-01-19 12:09:29 -0500

    Add skeleton for class variables

--- 09-oop-part-2/library/item.rb
+++ 09-oop-part-2/library/item.rb
@@ -1,9 +1,23 @@
 class Item
   
+  @@num_total       = 0
+  @@num_checked_out = 0
+  
   attr_reader :title
+
+  # Class Variable Reader
+  def self.num_total
+    @@num_total
+  end
+  
+  def self.num_checked_out
+    @@num_checked_out
+  end
 
   def initialize(title)
     @title = title
+    
+    @@num_total += 1
   end  
   
 end
--- 09-oop-part-2/library/main.rb
+++ 09-oop-part-2/library/main.rb
@@ -5,5 +5,8 @@ require './music'
 hp1 = Book.new('Harry Potter 1')
 b5  = Music.new("Beethoven's Fifth")
 
-puts hp1.read
-puts b5.play
+#puts hp1.read
+#puts b5.play
+
+puts "The library has #{ Item.num_total } items in its collection"
+puts "The library has loaned out #{ Item.num_checked_out } items"
