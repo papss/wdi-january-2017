24ce7a495a58170c79a13226b739891cd1b0ce1d
Joseph Papaluca <jpapaluc@gmail.com>
2017-01-27 11:30:00 -0500

    started a rails app

--- 14-intro-to-databases/film.rb
+++ 14-intro-to-databases/film.rb
@@ -1,28 +1,15 @@
-class Film
-  attr_accessor :id, :title, :country, :genre, :rating, :description
+gem 'activerecord', '= 4.2.7'
+require 'active_record'
+require 'mini_record'
 
-  @@library = []
-  @@id = 1;
+ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'films.sqlite3')
 
-  def initialize(options)
-    @title = options['title']
-    @country = options['country']
-    @genre = options['genre']
-    @rating = options['rating']
-    @description = options['description']
-    @id = @@id
-
-    @@id += 1
-  end
-
-  def self.create(options)
-    film = self.new(options)
-    @@library << film
-    return film
-  end
-
-  def self.all
-    return @@library
-  end
-
+class Film < ActiveRecord::Base
+  # attr_accessor :id, :title, :country, :genre, :rating, :description
+  field :title, as: :string
+  field :country, as: :string
+  field :genre, as: :string
+  field :rating, as: :integer
+  field :description, as: :text
 end
+Film.auto_upgrade! #if we add a field into our list of fields, it will be automatically reflected in DB
